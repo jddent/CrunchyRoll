71000877faf81d4ebb932c1dbedd79109ce72349
Jonathan Dent <jddent@mix.wvu.edu>
2022-01-25 13:50:21 -0500

    Completed top navigation bar, and created footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { topnavbar } from 'src/app/navigation/topnavbar.component';
-
+import { footer } from './navigation/footer.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    topnavbar
+    topnavbar,
+    footer
   ],
   imports: [
     BrowserModule
