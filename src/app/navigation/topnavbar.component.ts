9166b3567c46720057a0ded309479c218cf3ef88
Jonathan Dent <jddent@mix.wvu.edu>
2022-01-25 17:49:40 -0500

    Refactoring: top bar into links, allow collapsing, and expanding

--- src/app/navigation/topnavbar.component.ts
+++ src/app/navigation/topnavbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'cr-topnavbar',
-    templateUrl: 'topnavbar.component.html'
+    templateUrl: 'topnavbar.component.html',
 })
 export class topnavbar{
 
