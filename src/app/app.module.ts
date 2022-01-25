de2bab89dffc7b9f710c71ad5b60da02a1aca144
Jonathan Dent <jddent@mix.wvu.edu>
2022-01-25 14:14:47 -0500

    Created social footer, and updated logo to better fit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { topnavbar } from 'src/app/navigation/topnavbar.component';
 import { footer } from './navigation/footer.component';
+import { SocialFooter } from './navigation/social.footer.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     topnavbar,
-    footer
+    footer,
+    SocialFooter
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/social.footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cr-social-footer',
+    templateUrl: 'social.footer.component.html',
+    styleUrls: ['social.footer.component.html']
+})
+
+export class SocialFooter{
+    
+}
